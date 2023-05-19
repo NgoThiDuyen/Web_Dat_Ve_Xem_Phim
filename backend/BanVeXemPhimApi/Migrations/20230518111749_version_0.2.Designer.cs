a4e171b7016808c707cc386b7bab7026e175ab40
NgoThiDuyen <nguyenhua2508@gmail.com>
2023-05-19 21:11:50 +0700

    Change migration

--- backend/BanVeXemPhimApi/Migrations/20230518111749_version_0.2.Designer.cs
+++ backend/BanVeXemPhimApi/Migrations/20230518111749_version_0.2.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using BanVeXemPhimApi.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BanVeXemPhimApi.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230518111749_version_0.2")]
+    partial class version_02
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -50,19 +52,6 @@ namespace BanVeXemPhimApi.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("admins");
-
-                    b.HasData(
-                        new
-                        {
-                            Id = 1,
-                            CreatedDate = new DateTime(2023, 5, 19, 8, 22, 57, 598, DateTimeKind.Local).AddTicks(4669),
-                            Email = "admin@gmail.com",
-                            Name = "Admin",
-                            Password = "75D23AF433E0CEA4C0E45A56DBA18B30",
-                            Role = "Manager",
-                            UpdatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
-                            Username = "admin@gmail.com"
-                        });
                 });
 
             modelBuilder.Entity("BanVeXemPhimApi.Models.Cinema", b =>
@@ -143,14 +132,14 @@ namespace BanVeXemPhimApi.Migrations
                             Id = 1,
                             Author = "Gorō Taniguchi",
                             Cast = "Mayumi Tanaka (Luffy), Shuichi Ikeda (Shanks), Hiroaki Hirata (Sanji), Ikue Otani (Tony Tony Chopper)",
-                            CreatedDate = new DateTime(2023, 5, 19, 8, 22, 57, 597, DateTimeKind.Local).AddTicks(9455),
+                            CreatedDate = new DateTime(2023, 5, 18, 18, 17, 48, 952, DateTimeKind.Local).AddTicks(3232),
                             Description = "Bối cảnh One Piece Film Red diễn ra ở hòn đảo âm nhạc Elegia, nơi diva nổi tiếng bậc nhất thế giới tên Uta thực hiện buổi biểu diễn trực tiếp đầu tiên trước công chúng. Uta đứng trên sân khấu với một ước mơ giản dị rằng ” Âm nhạc của tôi sẽ khiến cho thế giới hạnh phúc”. Đằng sau hình ảnh cô ca sĩ sở hữu giọng hát được đánh giá là ở “Đẳng cấp hoàn toàn khác” là một thân thế vô cùng bí ẩn được che giấu. Băng hải tặc Mũ Rơm và các fan khác của Uta từ nhiều thế lực khác nhau như hải tặc lẫn hải quân đều đã cùng tề tựu về buổi biểu diễn này. Biến cố bắt đầu ngay khi sự thật kinh hoàng được tiết lộ rằng Uta chính là “con gái của Shanks”. Luffy và Uta lần đầu tiên hội ngộ sau lần gặp gỡ vào 12 năm trước tại Làng Foosha.",
                             Image = "image/movie/movie_1.jpg",
                             MovieType = 1,
                             Name = "ONE PIECE FILM RED",
                             NumberBooking = 0,
                             NumberView = 0,
-                            ReleaseDate = new DateTime(2023, 5, 19, 8, 22, 57, 598, DateTimeKind.Local).AddTicks(1399),
+                            ReleaseDate = new DateTime(2023, 5, 18, 18, 17, 48, 952, DateTimeKind.Local).AddTicks(4771),
                             Time = 115,
                             UpdatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
@@ -159,14 +148,14 @@ namespace BanVeXemPhimApi.Migrations
                             Id = 2,
                             Author = "Ryan Coogler",
                             Cast = "Tenoch Huerta, Martin Freeman, Lupita Nyong'o",
-                            CreatedDate = new DateTime(2023, 5, 19, 8, 22, 57, 598, DateTimeKind.Local).AddTicks(2192),
+                            CreatedDate = new DateTime(2023, 5, 18, 18, 17, 48, 952, DateTimeKind.Local).AddTicks(5681),
                             Description = "Bối cảnh One Piece Film Red diễn ra ở hòn đảo âm nhạc Elegia, nơi diva nổi tiếng bậc nhất thế giới tên Uta thực hiện buổi biểu diễn trực tiếp đầu tiên trước công chúng. Uta đứng trên sân khấu với một ước mơ giản dị rằng ” Âm nhạc của tôi sẽ khiến cho thế giới hạnh phúc”. Đằng sau hình ảnh cô ca sĩ sở hữu giọng hát được đánh giá là ở “Đẳng cấp hoàn toàn khác” là một thân thế vô cùng bí ẩn được che giấu. Băng hải tặc Mũ Rơm và các fan khác của Uta từ nhiều thế lực khác nhau như hải tặc lẫn hải quân đều đã cùng tề tựu về buổi biểu diễn này. Biến cố bắt đầu ngay khi sự thật kinh hoàng được tiết lộ rằng Uta chính là “con gái của Shanks”. Luffy và Uta lần đầu tiên hội ngộ sau lần gặp gỡ vào 12 năm trước tại Làng Foosha.",
                             Image = "image/movie/movie_2.jpg",
                             MovieType = 1,
                             Name = "CHIẾN BINH BÁO ĐEN 2: WAKANDA BẤT DIỆT",
                             NumberBooking = 0,
                             NumberView = 0,
-                            ReleaseDate = new DateTime(2023, 5, 19, 8, 22, 57, 598, DateTimeKind.Local).AddTicks(2197),
+                            ReleaseDate = new DateTime(2023, 5, 18, 18, 17, 48, 952, DateTimeKind.Local).AddTicks(5688),
                             Time = 115,
                             UpdatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
@@ -175,14 +164,14 @@ namespace BanVeXemPhimApi.Migrations
                             Id = 3,
                             Author = "James Lee",
                             Cast = "Alif Satar, Ikmal Amry, Abbas Mahmood",
-                            CreatedDate = new DateTime(2023, 5, 19, 8, 22, 57, 598, DateTimeKind.Local).AddTicks(2199),
+                            CreatedDate = new DateTime(2023, 5, 18, 18, 17, 48, 952, DateTimeKind.Local).AddTicks(5690),
                             Description = "Bối cảnh One Piece Film Red diễn ra ở hòn đảo âm nhạc Elegia, nơi diva nổi tiếng bậc nhất thế giới tên Uta thực hiện buổi biểu diễn trực tiếp đầu tiên trước công chúng. Uta đứng trên sân khấu với một ước mơ giản dị rằng ” Âm nhạc của tôi sẽ khiến cho thế giới hạnh phúc”. Đằng sau hình ảnh cô ca sĩ sở hữu giọng hát được đánh giá là ở “Đẳng cấp hoàn toàn khác” là một thân thế vô cùng bí ẩn được che giấu. Băng hải tặc Mũ Rơm và các fan khác của Uta từ nhiều thế lực khác nhau như hải tặc lẫn hải quân đều đã cùng tề tựu về buổi biểu diễn này. Biến cố bắt đầu ngay khi sự thật kinh hoàng được tiết lộ rằng Uta chính là “con gái của Shanks”. Luffy và Uta lần đầu tiên hội ngộ sau lần gặp gỡ vào 12 năm trước tại Làng Foosha.",
                             Image = "image/movie/movie_3.jpg",
                             MovieType = 4,
                             Name = "CHÚ NGUYỀN TÁI SINH",
                             NumberBooking = 0,
                             NumberView = 0,
-                            ReleaseDate = new DateTime(2023, 5, 19, 8, 22, 57, 598, DateTimeKind.Local).AddTicks(2201),
+                            ReleaseDate = new DateTime(2023, 5, 18, 18, 17, 48, 952, DateTimeKind.Local).AddTicks(5691),
                             Time = 115,
                             UpdatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
@@ -191,14 +180,14 @@ namespace BanVeXemPhimApi.Migrations
                             Id = 4,
                             Author = "Nguyễn Chung",
                             Cast = "NSND Kim Xuân, NSƯT Lê Thiện, NSƯT Công Ninh, Dược Sĩ Tiến, Phạm Huỳnh Hữu Tài",
-                            CreatedDate = new DateTime(2023, 5, 19, 8, 22, 57, 598, DateTimeKind.Local).AddTicks(2202),
+                            CreatedDate = new DateTime(2023, 5, 18, 18, 17, 48, 952, DateTimeKind.Local).AddTicks(5692),
                             Description = "Bối cảnh One Piece Film Red diễn ra ở hòn đảo âm nhạc Elegia, nơi diva nổi tiếng bậc nhất thế giới tên Uta thực hiện buổi biểu diễn trực tiếp đầu tiên trước công chúng. Uta đứng trên sân khấu với một ước mơ giản dị rằng ” Âm nhạc của tôi sẽ khiến cho thế giới hạnh phúc”. Đằng sau hình ảnh cô ca sĩ sở hữu giọng hát được đánh giá là ở “Đẳng cấp hoàn toàn khác” là một thân thế vô cùng bí ẩn được che giấu. Băng hải tặc Mũ Rơm và các fan khác của Uta từ nhiều thế lực khác nhau như hải tặc lẫn hải quân đều đã cùng tề tựu về buổi biểu diễn này. Biến cố bắt đầu ngay khi sự thật kinh hoàng được tiết lộ rằng Uta chính là “con gái của Shanks”. Luffy và Uta lần đầu tiên hội ngộ sau lần gặp gỡ vào 12 năm trước tại Làng Foosha.",
                             Image = "image/movie/movie_4.jpg",
                             MovieType = 4,
                             Name = "HẠNH PHÚC MÁU",
                             NumberBooking = 0,
                             NumberView = 0,
-                            ReleaseDate = new DateTime(2023, 5, 19, 8, 22, 57, 598, DateTimeKind.Local).AddTicks(2204),
+                            ReleaseDate = new DateTime(2023, 5, 18, 18, 17, 48, 952, DateTimeKind.Local).AddTicks(5694),
                             Time = 115,
                             UpdatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
@@ -207,14 +196,14 @@ namespace BanVeXemPhimApi.Migrations
                             Id = 5,
                             Author = "Samuel Tourneux",
                             Cast = "",
-                            CreatedDate = new DateTime(2023, 5, 19, 8, 22, 57, 598, DateTimeKind.Local).AddTicks(2205),
+                            CreatedDate = new DateTime(2023, 5, 18, 18, 17, 48, 952, DateTimeKind.Local).AddTicks(5695),
                             Description = "Bối cảnh One Piece Film Red diễn ra ở hòn đảo âm nhạc Elegia, nơi diva nổi tiếng bậc nhất thế giới tên Uta thực hiện buổi biểu diễn trực tiếp đầu tiên trước công chúng. Uta đứng trên sân khấu với một ước mơ giản dị rằng ” Âm nhạc của tôi sẽ khiến cho thế giới hạnh phúc”. Đằng sau hình ảnh cô ca sĩ sở hữu giọng hát được đánh giá là ở “Đẳng cấp hoàn toàn khác” là một thân thế vô cùng bí ẩn được che giấu. Băng hải tặc Mũ Rơm và các fan khác của Uta từ nhiều thế lực khác nhau như hải tặc lẫn hải quân đều đã cùng tề tựu về buổi biểu diễn này. Biến cố bắt đầu ngay khi sự thật kinh hoàng được tiết lộ rằng Uta chính là “con gái của Shanks”. Luffy và Uta lần đầu tiên hội ngộ sau lần gặp gỡ vào 12 năm trước tại Làng Foosha.",
                             Image = "image/movie/movie_5.jpg",
                             MovieType = 3,
                             Name = "KHỈ CON LON TON THẾ GIỚI",
                             NumberBooking = 0,
                             NumberView = 0,
-                            ReleaseDate = new DateTime(2023, 5, 19, 8, 22, 57, 598, DateTimeKind.Local).AddTicks(2206),
+                            ReleaseDate = new DateTime(2023, 5, 18, 18, 17, 48, 952, DateTimeKind.Local).AddTicks(5696),
                             Time = 115,
                             UpdatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         });
@@ -307,7 +296,7 @@ namespace BanVeXemPhimApi.Migrations
                         new
                         {
                             Id = 1,
-                            CreatedDate = new DateTime(2023, 5, 19, 8, 22, 57, 592, DateTimeKind.Local).AddTicks(2458),
+                            CreatedDate = new DateTime(2023, 5, 18, 18, 17, 48, 947, DateTimeKind.Local).AddTicks(512),
                             Name = "Nguyễn Văn Test",
                             Password = "3213D1FD1AEAE020606E12C32EAFE45E",
                             UpdatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
---
+++ backend/BanVeXemPhimApi/Migrations/20230518111749_version_0.2.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BanVeXemPhimApi.Migrations
+{
+    public partial class version_02 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "reviewMovies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    MovieId = table.Column<int>(type: "int", nullable: false),
+                    Content = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CreatedDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_reviewMovies", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "movies",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "ReleaseDate" },
+                values: new object[] { new DateTime(2023, 5, 18, 18, 17, 48, 952, DateTimeKind.Local).AddTicks(3232), new DateTime(2023, 5, 18, 18, 17, 48, 952, DateTimeKind.Local).AddTicks(4771) });
+
+            migrationBuilder.UpdateData(
+                table: "movies",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "ReleaseDate" },
+                values: new object[] { new DateTime(2023, 5, 18, 18, 17, 48, 952, DateTimeKind.Local).AddTicks(5681), new DateTime(2023, 5, 18, 18, 17, 48, 952, DateTimeKind.Local).AddTicks(5688) });
+
+            migrationBuilder.UpdateData(
+                table: "movies",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedDate", "ReleaseDate" },
+                values: new object[] { new DateTime(2023, 5, 18, 18, 17, 48, 952, DateTimeKind.Local).AddTicks(5690), new DateTime(2023, 5, 18, 18, 17, 48, 952, DateTimeKind.Local).AddTicks(5691) });
+
+            migrationBuilder.UpdateData(
+                table: "movies",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "CreatedDate", "ReleaseDate" },
+                values: new object[] { new DateTime(2023, 5, 18, 18, 17, 48, 952, DateTimeKind.Local).AddTicks(5692), new DateTime(2023, 5, 18, 18, 17, 48, 952, DateTimeKind.Local).AddTicks(5694) });
+
+            migrationBuilder.UpdateData(
+                table: "movies",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "CreatedDate", "ReleaseDate" },
+                values: new object[] { new DateTime(2023, 5, 18, 18, 17, 48, 952, DateTimeKind.Local).AddTicks(5695), new DateTime(2023, 5, 18, 18, 17, 48, 952, DateTimeKind.Local).AddTicks(5696) });
+
+            migrationBuilder.UpdateData(
+                table: "users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 5, 18, 18, 17, 48, 947, DateTimeKind.Local).AddTicks(512));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "reviewMovies");
+
+            migrationBuilder.UpdateData(
+                table: "movies",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "ReleaseDate" },
+                values: new object[] { new DateTime(2023, 4, 11, 5, 34, 37, 550, DateTimeKind.Utc).AddTicks(1959), new DateTime(2023, 4, 11, 12, 34, 37, 550, DateTimeKind.Local).AddTicks(6652) });
+
+            migrationBuilder.UpdateData(
+                table: "movies",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "ReleaseDate" },
+                values: new object[] { new DateTime(2023, 4, 11, 5, 34, 37, 553, DateTimeKind.Utc).AddTicks(639), new DateTime(2023, 4, 11, 12, 34, 37, 553, DateTimeKind.Local).AddTicks(653) });
+
+            migrationBuilder.UpdateData(
+                table: "movies",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedDate", "ReleaseDate" },
+                values: new object[] { new DateTime(2023, 4, 11, 5, 34, 37, 553, DateTimeKind.Utc).AddTicks(861), new DateTime(2023, 4, 11, 12, 34, 37, 553, DateTimeKind.Local).AddTicks(867) });
+
+            migrationBuilder.UpdateData(
+                table: "movies",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "CreatedDate", "ReleaseDate" },
+                values: new object[] { new DateTime(2023, 4, 11, 5, 34, 37, 553, DateTimeKind.Utc).AddTicks(872), new DateTime(2023, 4, 11, 12, 34, 37, 553, DateTimeKind.Local).AddTicks(880) });
+
+            migrationBuilder.UpdateData(
+                table: "movies",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "CreatedDate", "ReleaseDate" },
+                values: new object[] { new DateTime(2023, 4, 11, 5, 34, 37, 553, DateTimeKind.Utc).AddTicks(882), new DateTime(2023, 4, 11, 12, 34, 37, 553, DateTimeKind.Local).AddTicks(885) });
+
+            migrationBuilder.UpdateData(
+                table: "users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 4, 11, 5, 34, 37, 532, DateTimeKind.Utc).AddTicks(7170));
+        }
+    }
+}
