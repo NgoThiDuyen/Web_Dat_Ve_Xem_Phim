98c956ca0693c5831394dbbaa7b56928fd0447b3
NgoThiDuyen <duyen.nt207669@sis.hust.edu.vn>
2023-06-18 20:37:09 +0700

    Config to prepare for api download

--- backend/BanVeXemPhimApi/Common/ApiOption.cs
+++ backend/BanVeXemPhimApi/Common/ApiOption.cs
@@ -6,5 +6,6 @@
         public string ValidAudience { get; set; }
         public string ValidIssuer { get; set; }
         public string Secret { get; set; }
+        public string BaseUrl { get; set; }
     }
 }
